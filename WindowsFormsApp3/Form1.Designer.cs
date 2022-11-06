701153442fefe55fd271a0dd9e06d39aaffb500e
rukiyecyrbg <92246250+rukiyecyrbg@users.noreply.github.com>
2022-11-07 01:05:01 +0300

    Add files via upload

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pictureBox1.Location = new System.Drawing.Point(59, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(135, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(218, 56);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(136, 102);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(372, 56);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(142, 102);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(59, 176);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(135, 102);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(218, 176);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(136, 102);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(372, 176);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(142, 102);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(59, 300);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(135, 90);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Location = new System.Drawing.Point(218, 300);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(136, 90);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(372, 300);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(142, 90);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public char secimtipi = 'X';
+        public Form1()
+        {
+            InitializeComponent();
+
+            //pictureBox1.Load("./img/o.jfif");
+            //pictureBox1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender;
+            if (pb.Image != null) return;
+
+            if (secimtipi == 'X')
+            {
+                pb.Load("./img/X.jfif");
+                pb.Invalidate();
+                pb.Tag = secimtipi;
+                secimtipi = 'o';
+            }
+            else
+            {
+                pb.Load("./img/o.jfif");
+                pb.Invalidate();
+                pb.Tag = secimtipi;
+                secimtipi = 'X';
+            }
+			kontrol();
+            //if (pb.Name == "pictureBox1")
+            //{
+            //    if (Convert.ToString(pb.Tag) == Convert.ToString(pictureBox2.Tag) && Convert.ToString(pictureBox3.Tag))
+            //    {
+            //        MessageBox.Show("kazandınız");
+            //    }
+            //}
+
+        }
+        public void kontrol()
+        {
+			MessageBox.Show(pictureBox1.Tag.ToString());
+            if (pictureBox1.Tag.ToString() == "x" && pictureBox2.Tag.ToString() == "x" && pictureBox3.Tag.ToString() == "x")
+			{
+				MessageBox.Show("X KAZANDI");
+			}
+			
+			//if (bt4.Text == "X" && bt5.Text == "X" && bt6.Text == "X")
+			//{
+			//	MessageBox.Show("X KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt7.Text == "X" && bt8.Text == "X" && bt9.Text == "X")
+			//{
+			//	MessageBox.Show("X KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt1.Text == "X" && bt4.Text == "X" && bt7.Text == "X")
+			//{
+			//	MessageBox.Show("X KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt2.Text == "X" && bt5.Text == "X" && bt8.Text == "X")
+			//{
+			//	MessageBox.Show("X KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt3.Text == "X" && bt6.Text == "X" && bt9.Text == "X")
+			//{
+			//	MessageBox.Show("X KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt1.Text == "X" && bt5.Text == "X" && bt9.Text == "X")
+			//{
+			//	MessageBox.Show("X KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt3.Text == "X" && bt5.Text == "X" && bt7.Text == "X")
+			//{
+			//	MessageBox.Show("X KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt1.Text == "O" && bt2.Text == "O" && bt3.Text == "O")
+			//{
+			//	MessageBox.Show("O KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt4.Text == "O" && bt5.Text == "O" && bt6.Text == "O")
+			//{
+			//	MessageBox.Show("O KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt7.Text == "O" && bt8.Text == "O" && bt9.Text == "O")
+			//{
+			//	MessageBox.Show("O KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt1.Text == "O" && bt4.Text == "O" && bt7.Text == "O")
+			//{
+			//	MessageBox.Show("O KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt2.Text == "O" && bt5.Text == "O" && bt8.Text == "O")
+			//{
+			//	MessageBox.Show("O KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt3.Text == "O" && bt6.Text == "O" && bt9.Text == "O")
+			//{
+			//	MessageBox.Show("O KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt1.Text == "O" && bt5.Text == "O" && bt9.Text == "O")
+			//{
+			//	MessageBox.Show("O KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//if (bt3.Text == "O" && bt5.Text == "O" && bt7.Text == "O")
+			//{
+			//	MessageBox.Show("O KAZANDI");
+			//	yeniOyun();
+			//}
+			
+			//else if (bt1.Text != "" && bt2.Text != "" && bt3.Text != "" && bt4.Text != "" && bt5.Text != "" && bt6.Text != "" && bt7.Text != "" && bt8.Text != "" && bt9.Text != "")
+			//{
+			//	MessageBox.Show("KAZANAN YOK");
+			//	yeniOyun();
+			//}
+        }
+    }
+}
